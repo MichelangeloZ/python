a9f90f51aff2984c98188b6c991edaa3d11da7bf
unknown <38968@ehi.com.cn>
2021-05-14 17:54:25 +0800

    函数传参

---
+++ python/function.py
@@ -0,0 +1,53 @@
+#  #_*_coding:utf-8 _*_
+# def greet(name):
+#     print("Hello! " + name)
+# greet("Jessica")
+
+# def display_message():
+#     print("I have learnt function")
+# display_message()
+
+# def favorite_book(title):
+#     print("One of my favorite books is " + title)
+# favorite_book("Alice in Wonderland")
+
+# def describe_pet(animal_type, pet_name):
+#     """显示宠物的信息"""
+#     print("\nI have a " + animal_type + ".")
+#     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# describe_pet('hamster', 'harry')
+# describe_pet('dog', 'willie')
+# describe_pet(animal_type='cat', pet_name='Ketty')
+
+
+# # 形参指定默认值
+# def describe_pet(pet_name, animal_type='dog'):
+#     """显示宠物的信息"""
+#     print("\nI have a " + animal_type + ".")
+#     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# describe_pet(pet_name='willie')
+# describe_pet('willie')
+
+# def make_shirt(size, word):
+#     print("\nT-shirt's size is " + size)
+#     print("there is " + word + " on the T-shirt.")
+# make_shirt(size="S", word="LINING" )
+
+
+# def make_shirt(size="L", word="I love Python"):
+#     print("\nT-shirt's size is " + size)
+#     print("there is " + word + " on the T-shirt.")
+# make_shirt()
+# make_shirt("M")
+# make_shirt(word="Good Morning")
+
+def describle_city(city_name, country_name="China"):
+    print(city_name + " is in " + country_name)
+describle_city("shanghai")
+describle_city(city_name="wuhan")
+describle_city("Reykjavik", "Iceland")
+
+shanghai is in China
+wuhan is in China
+Reykjavik is in Iceland
+
--- python/while.py
+++ python/while.py
@@ -93,7 +93,7 @@ KeyboardInterrupt
 pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
 print(pets)
 while 'cat' in pets:
-pets.remove('cat')
+  pets.remove('cat')
 print(pets)
 
 ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
