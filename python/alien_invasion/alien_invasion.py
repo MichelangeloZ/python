d23d9e2f035df3421bae4c3f558c8c92a5b70baa
unknown <38968@ehi.com.cn>
2021-06-02 15:29:29 +0800

    外星人游戏

---
+++ python/alien_invasion/alien_invasion.py
@@ -0,0 +1,27 @@
+# _*_coding:utf-8 _*_
+
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(ai_settings.screen_width, ai_settings.screen_height)
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(ai_settings.bg_color)
+
+        ship.blitme()
+
+        pygame.display.flip()
+        
+run_game()
+
---
+++ python/alien_invasion/settings.py
@@ -0,0 +1,10 @@
+# _*_coding:utf-8 _*_
+
+import sys
+import pygame
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
